23df80fcba8433a95b89d53df76a36cd409dd420
victortsrodrigues <victadeu@gmail.com>
2025-01-18 10:23:32 -0300

    Implementado GET de transações

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -3,13 +3,28 @@ import httpStatus from "http-status";
 
 export async function creatTransaction(req, res) {
   const transaction = req.body;
-
   try {
     await db.collection("transactions").insertOne({
       ...transaction,
       user: res.locals.user._id
     });
     res.status(httpStatus.CREATED).send("Transaction created!");
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+  }
+}
+
+export async function readTransactions(req, res) {
+  const page = req.query.page || 1;
+  if (page <= 0) {
+    return res.status(httpStatus.BAD_REQUEST).send("Page number must a positive integer.");
+  }
+  const limit = 10;
+  const skip = (page - 1) * limit;
+  try {
+    const transactions = await db.collection("transactions").find({ user: res.locals.user._id }).skip(skip).limit(limit).sort({ _id: -1 }).toArray();
+    console.log(transactions)
+    return res.status(httpStatus.OK).send(transactions);
   } catch (error) {
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
   }
--- src/routers/transactionsRouter.js
+++ src/routers/transactionsRouter.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/schemaMiddleware.js";
 import { transactionSchema } from "../schemas/transactionSchema.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
-import { creatTransaction } from "../controllers/transactionsController.js";
+import { creatTransaction, readTransactions } from "../controllers/transactionsController.js";
 
 const transactionsRouter = Router();
 
 transactionsRouter.use(validateToken);
 transactionsRouter.post("/transaction", validateSchema(transactionSchema), creatTransaction);
+transactionsRouter.get("/transaction", readTransactions);
 
 export default transactionsRouter;
