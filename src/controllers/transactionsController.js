c5cf9e4159638fd07df5fbc5d1fcbf36590f9a5f
victortsrodrigues <victadeu@gmail.com>
2025-01-18 11:30:11 -0300

    implementado DELETE de transações

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -17,12 +17,14 @@ export async function creatTransaction(req, res) {
 
 export async function readTransactions(req, res) {
   const page = req.query.page || 1;
-  if (page <= 0) {
+  // Check if page query is a positive
+  if (page <= 0 || !Number.isInteger(page)) {
     return res.status(httpStatus.BAD_REQUEST).send("Page number must a positive integer.");
   }
   const limit = 10;
   const skip = (page - 1) * limit;
   try {
+    // Read sorted transactions
     const transactions = await db.collection("transactions").find({ user: res.locals.user._id }).skip(skip).limit(limit).sort({ _id: -1 }).toArray();
     return res.status(httpStatus.OK).send(transactions);
   } catch (error) {
@@ -36,7 +38,7 @@ export async function updateTransactions(req, res) {
   try {
     // Check if transaction exists
     const registeredTransaction = await db.collection("transactions").findOne({ _id: new ObjectId(id) });
-    if(!registeredTransaction) {
+    if (!registeredTransaction) {
       return res.status(httpStatus.NOT_FOUND).send("Transaction not found!");
     }
     // Check if user is the owner of the transaction
@@ -45,14 +47,36 @@ export async function updateTransactions(req, res) {
     }
     // Update transaction
     await db.collection("transactions").updateOne(
-      {_id: new ObjectId(id)},
-      {$set: {
-        value: transaction.value,
-        description: transaction.description,
-        type: transaction.type
-      }}
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          value: transaction.value,
+          description: transaction.description,
+          type: transaction.type
+        }
+      }
     );
     res.status(httpStatus.NO_CONTENT).send('Transaction updated!');
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+  }
+}
+
+export async function deleteTransaction(req, res) {
+  const { id } = req.params;
+  try {
+    // Check if transaction exists
+    const registeredTransaction = await db.collection("transactions").findOne({ _id: new ObjectId(id) });
+    if (!registeredTransaction) {
+      return res.status(httpStatus.NOT_FOUND).send("Transaction not found!");
+    }
+    // Check if user is the owner of the transaction
+    if (!registeredTransaction.user.equals(res.locals.user._id)) {
+      return res.status(httpStatus.UNAUTHORIZED).send('You can only delete your own transactions!');
+    }
+    // Delete transaction
+    await db.collection("transactions").deleteOne({ _id: new ObjectId(id) });
+    res.status(httpStatus.NO_CONTENT).send('Transaction deleted!');
   } catch (error) {
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
   }
--- src/routers/transactionsRouter.js
+++ src/routers/transactionsRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/schemaMiddleware.js";
 import { transactionSchema } from "../schemas/transactionSchema.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
-import { creatTransaction, readTransactions, updateTransactions } from "../controllers/transactionsController.js";
+import { creatTransaction, deleteTransaction, readTransactions, updateTransactions } from "../controllers/transactionsController.js";
 
 const transactionsRouter = Router();
 
@@ -10,5 +10,6 @@ transactionsRouter.use(validateToken);
 transactionsRouter.post("/transaction", validateSchema(transactionSchema), creatTransaction);
 transactionsRouter.get("/transaction", readTransactions);
 transactionsRouter.put("/transaction/:id", validateSchema(transactionSchema), updateTransactions)
+transactionsRouter.delete("/transaction/:id", deleteTransaction);
 
 export default transactionsRouter;
